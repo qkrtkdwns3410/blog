2ead982aa107ee084ab9b8a1859ad64a8bbd935f
psj <qkrtkdwns3410@naver.com>
2021-06-13 16:37:57 +0900

    6-10

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -3,12 +3,17 @@ package com.cos.blog.test;
 import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.UserRepository;
+import jdk.nashorn.internal.runtime.linker.LinkerCallSite;
+import org.hibernate.engine.jdbc.Size;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
+import java.util.List;
 import java.util.function.Supplier;
 
 
@@ -20,7 +25,43 @@ public class DummyControllerTest {
     private UserRepository userRepository;
     
     
-    //{id}wnthfh 파라메터를 전달 받을 수 있습니다.
+    @GetMapping("/dummy/users")
+    public List<User> list() {
+        return userRepository.findAll();
+    }
+    
+    @GetMapping("/dummy/user")
+    public List<User> pageList(@PageableDefault(size = 2, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+    
+        Page<User> pagingUser= userRepository.findAll(pageable);
+        List<User> users = pagingUser.getContent();
+        return users;
+    }
+    
+    //save함수는 id를 전달하면 해당 id에 대한 데이터가 있으면 update를 해주고
+    //save함수는 id를 전달하면 해당 id에 대한 데이터가 없으면 insert
+    @Transactional //더티 체킹. save를 하지 않아도 update가 가능합니다.
+    @PutMapping("/dummy/user/{id}")
+    public User updateUser(@PathVariable int id, @RequestBody User requestUser) { //json 데이터를 요청 >> Java Object(Message Converter의 Jackson 라이브러리가 변환해서  받아줍니다.
+        System.out.println("id " + id);
+        System.out.println("password " + requestUser.getPassword());
+        System.out.println("email " + requestUser.getEmail());
+        
+        
+        User user = userRepository.findById(id).orElseThrow(() -> {
+            return new IllegalArgumentException("수정에 실패하였습니다.");
+        });
+        user.setPassword(requestUser.getPassword());
+        user.setEmail(requestUser.getEmail());
+        
+//        userRepository.save(user);
+        return null;
+        
+    }
+    
+    
+    
+    //{id}주소 파라메터를 전달 받을 수 있습니다.
     //http://localhost:8000/blog/dummy/user/3
     @GetMapping("/dummy/user/{id}")
     public User detail(@PathVariable int id) {
