abf8e649ea516322355523a97d5da7cb6a2c3c8d
psj <qkrtkdwns3410@naver.com>
2021-06-16 16:32:03 +0900

    6-10

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -19,13 +19,17 @@ let index = {
         $.ajax({
             //회원가입 수행 요청
             type: "POST",
-            url: "/api/user",
-            data: JSON.stringify(data)
+            url: "/blog/api/user",
+            data: JSON.stringify(data),
+            contentType: "application/json;charset=UTF-8", //body데이터가 어떤 타입인지 (MIME)
+            dataType: "json" //요청을 서버로 해서 응답이 왔을경우 기본적으로 모든 것이 문자열 입니다.(생긴게 json이라면 >>javascript
 
-        }).done(function () {
-
-        }).fail(function () {
-
+        }).done(function (resp) {
+            alert("회원가입이 완료되었습니다.");
+            alert(resp);
+            location.href = "/blog";
+        }).fail(function (error) {
+            alert(JSON.stringify(arror));
         }); // ajax통신을 이용해서 3개의 데이터를 json으로 변경하여 insert요청합니다.
     }
 
