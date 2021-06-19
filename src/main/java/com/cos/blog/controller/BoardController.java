82d2a35edb77f099f0f35019249aebdea70c8492
psj <qkrtkdwns3410@naver.com>
2021-06-19 15:40:06 +0900

    6-10

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -10,8 +10,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class BoardController {
     
     @GetMapping({"", "/"})
-    public String index(@AuthenticationPrincipal PrincipalDetail principalDetail) { //컨트롤러에서 세션을 어떻게 찾는가?
-        System.out.println("로그인 사용자 아이디: " + principalDetail.getUsername());
+    public String index() { //컨트롤러에서 세션을 어떻게 찾는가?
         return "index";
     }
+    
+    //USER의 권한이 필요합니다.
+    @GetMapping("/board/saveForm")
+    public String saveForm() {
+        return "board/savaForm";
+    }
+    
 }
