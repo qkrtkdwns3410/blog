20109ce75b2234b2aac34e7080e4ef4f84083f67
psj <qkrtkdwns3410@naver.com>
2021-06-22 15:34:42 +0900

    6-10

--- src/main/java/com/cos/blog/service/UserService.java
+++ src/main/java/com/cos/blog/service/UserService.java
@@ -29,5 +29,15 @@ public class UserService {
         user.setRole(RoleType.USER);
         userRepository.save(user);
     }
-    
+    @Transactional
+    public void 회원수정(User user) {
+        //수정시에는 영속성 컨텍스트 User 오브젝트를 영속화 시키고, 영속화된 User오브젝트를 수정
+        //select를 해서 User오브젝트를 DB로 부터 가져오는 이유는 영속화를 하기 위해서
+        //영속화된 오브젝트를 변경하면 자동으로 DB에 update문을 날려준다.
+        User persistance = userRepository.findById(user.getId()).orElseThrow(() -> {
+            return new IllegalArgumentException("회원 찾기 실패");
+        });
+        String rawPassword = user.getPassword();
+        
+    }
 }
--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -3,6 +3,9 @@ let index = {
         $("#btn_save").on("click", () => { //function(){} , () = >{} this를 바인딩하기 위하여
             this.save();
         });
+        $("#btn_update").on("click", () => { //function(){} , () = >{} this를 바인딩하기 위하여
+            this.update();
+        });
     },
 
     save: function () {
@@ -27,6 +30,31 @@ let index = {
 
         }).done(function (resp) {
             alert("회원가입이 완료되었습니다.");
+            console.log(resp);
+            location.href = "/";
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        }); // ajax통신을 이용해서 3개의 데이터를 json으로 변경하여 insert요청합니다.
+    },
+    update: function () {
+        // alert('user의 save함수 호출됨');
+        let data = {
+            id: $("#id").val(),
+            password: $("#password").val(),
+            email: $("#email").val()
+        };
+
+
+        $.ajax({
+            //회원가입 수행 요청
+            type: "PUT",
+            url: "/user",
+            data: JSON.stringify(data),
+            contentType: "application/json;charset=UTF-8", //body데이터가 어떤 타입인지 (MIME)
+            dataType: "json" //요청을 서버로 해서 응답이 왔을경우 기본적으로 모든 것이 문자열 입니다.(생긴게 json이라면 >>javascript)
+
+        }).done(function (resp) {
+            alert("회원수정이 완료되었습니다.");
             console.log(resp);
             location.href = "/";
         }).fail(function (error) {
